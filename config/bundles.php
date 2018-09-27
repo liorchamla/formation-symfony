ddb11f0481199ac6bde2982c06d2f2d59c327e61
Lior Chamla <lchamla@gmail.com>
2018-09-27 13:59:11 +0200

    Ajout des fixtures (temporaire)

--- config/bundles.php
+++ config/bundles.php
@@ -14,5 +14,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
 ];
