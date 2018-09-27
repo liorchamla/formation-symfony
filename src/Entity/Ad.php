1a66a719a2694e56c585dba1d02a0f2e1a0e70f8
Lior Chamla <lchamla@gmail.com>
2018-09-27 13:11:32 +0200

    Ajout de la date de création dans les Ad

--- src/Entity/Ad.php
+++ src/Entity/Ad.php
@@ -89,6 +89,11 @@ class Ad
      */
     private $comments;
 
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $createdAt;
+
     public function __construct()
     {
         $this->images = new ArrayCollection();
@@ -358,6 +363,18 @@ class Ad
                 $comment->setAd(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
 
         return $this;
     }
